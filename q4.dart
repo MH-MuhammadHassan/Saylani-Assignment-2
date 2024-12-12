1229b03fafd0ff0ed1f2899a09735620a676eff9
HassanCodeWave <shforshaikhhassan@gmail.com>
2024-12-12 09:25:06 +0500

    .first method added to get value of first item

--- q4.dart
+++ q4.dart
@@ -2,9 +2,9 @@
 // number from a list.
 
 void main() {
-  List<num> numbers = [2, 3, 1,22,56,101,53,66];
+  List<num> numbers = [2, 3, 1, 22, 56, 101, 53, 66];
   numbers.sort();
   print(numbers);
-  print("Smallest Number is: ${numbers[0]}");
+  print("Smallest Number is: ${numbers.first}");
   print("Greatest Number is: ${numbers.last}");
 }
