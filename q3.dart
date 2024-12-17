a90544b21d9ed64deaea0cd1cd5f5db9bae20099
HassanCodeWave <shforshaikhhassan@gmail.com>
2024-12-18 00:48:45 +0500

    use while loop for removing days line by line

--- q3.dart
+++ q3.dart
@@ -10,25 +10,32 @@ void main() {
     "Friday",
     "Saturday"
   ];
-  days.remove("Sunday");
-  print("List After removing Sunday: $days");
-  print("--------------------");
-  days.remove("Monday");
-  print("List After removing Monday: $days");
-  print("--------------------");
-  days.remove("Tuesday");
-  print("List After removing Tuesday: $days");
-  print("--------------------");
-  days.remove("Wednesday");
-  print("List After removing Wednesday: $days");
-  print("--------------------");
-  days.remove("Thursday");
-  print("List After removing Thursday: $days");
-  print("--------------------");
-  days.remove("Friday");
-  print("List After removing Friday: $days");
-  print("--------------------");
-  days.remove("Saturday");
-  print("List After removing Saturday: $days");
-  print("--------------------");
+
+  while (days.isNotEmpty) {
+    var remove = days.removeLast();
+    print("Day Removed: $remove");
+    print("Remaining list: $days");
+  }
+
+  // days.remove("Sunday");
+  // print("List After removing Sunday: $days");
+  // print("--------------------");
+  // days.remove("Monday");
+  // print("List After removing Monday: $days");
+  // print("--------------------");
+  // days.remove("Tuesday");
+  // print("List After removing Tuesday: $days");
+  // print("--------------------");
+  // days.remove("Wednesday");
+  // print("List After removing Wednesday: $days");
+  // print("--------------------");
+  // days.remove("Thursday");
+  // print("List After removing Thursday: $days");
+  // print("--------------------");
+  // days.remove("Friday");
+  // print("List After removing Friday: $days");
+  // print("--------------------");
+  // days.remove("Saturday");
+  // print("List After removing Saturday: $days");
+  // print("--------------------");
 }
