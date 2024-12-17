b48f364034ce14ef662a7a919da00da1cf5e4b0d
HassanCodeWave <shforshaikhhassan@gmail.com>
2024-12-18 00:47:14 +0500

    update simple print to forEach to show the list items step by step

--- q1.dart
+++ q1.dart
@@ -1,5 +1,5 @@
 // Q.1: Create a list of names and print all names using list.
 void main() {
   List<String> names = ["Hassan", "Ali", "Usama"];
-  print(names);
+  names.forEach((action) => print(action));
 }
